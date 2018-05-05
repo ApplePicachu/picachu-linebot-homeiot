84acaa32cf717b715fdaaf150a3154e11aa090d1
Picachu Wu <q8139q8139@gmail.com>
2018-05-05 22:13:06 +0800

    Create linebot

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-var express = require('express');
-const app = express();
+const Express = require('express');
+const { Client } = require('pg');//Postgres
+const Linebot = require('linebot');//Line Bot API
+
+//Postgres connect
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+client.connect();
+
+//Create linebot parser
+var bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+        // success
+    }).catch(function (error) {
+        // error
+    });
+});
+const linebotParser = bot.parser();
+
+//Express init.
+const app = Express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/linewebhook', linebotParser);
 app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'));
