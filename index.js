2ca9c1d58010e5fdda869f2ffea4cf0ec3ab2eba
Picachu Wu <q8139q8139@gmail.com>
2018-05-06 17:50:44 +0800

    Modify insert process

--- index.js
+++ index.js
@@ -25,61 +25,63 @@ const app = Express();
 
 app.post('/ngrok/url', (req, res) => {
     console.log(req.headers.data);
-    sqlManager.insertSetting({key:'ngrok_url', value:req.headers.data}, (err, sqlRes) => {
-        if (err) {
-            console.log('Error.\n'+err.stack);
-        } else {
-            console.log('Success.\n' + JSON.stringify(sqlRes));
-            var options = {
-                url: req.headers.data,
-                method: 'GET',
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            };
-            request(options, (reqErr, reqRes, body) => {
-                if (!reqErr && reqRes.statusCode == 200){
-                    console.log(body);
+    var options = {
+        url: req.headers.data,
+        method: 'GET',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    };
+    request(options, (reqErr, reqRes, body) => {
+        if (!reqErr && reqRes.statusCode == 200) {
+            console.log(body);
+            sqlManager.insertSetting({ key: 'ngrok_url', value: req.headers.data }, (sqlErr, sqlRes) => {
+                if (sqlErr) {
+                    console.log('Error.\n' + sqlErr.stack);
+                } else {
+                    console.log('Success.\n' + JSON.stringify(sqlRes));
+                    res.send('Success');
                 }
-                res.send('Success');
             });
-        }
-    });
-    
-})
-app.get('/sql/settings/:setting_key', (req, res) => {
-    sqlManager.getSetting(req.params.setting_key, (err, sqlRes) => {
-        if (err) {
-            console.log('Error.\n'+err.stack);
+        } else if (reqErr) {
+            console.log('Error.\n' + reqErr.stack);
         } else {
-            console.log('Success.\n' + JSON.stringify(sqlRes));
-            res.send(sqlRes);
+            console.log('Fail with error code: \n' + reqRes.statusCode);
         }
-    });
-});
-app.post('/', linebotParser);
-
-const connectSqlAsyncRun = async() => {
-    const client = new Client({ connectionString: process.env.DATABASE_URL, });
-    await client.connect();
-    //Init sql manager
-    sqlManager = new SqlManager(client);
-    return sqlManager;
-}
-connectSqlAsyncRun().then((sqlManager) => {
-    console.log('SQL connect success. App start.');
-    var server = app.listen(process.env.PORT || 8080, () => {
-        var port = server.address().port;
-        console.log("App now running on port", port);
-    
-        sqlManager.createTables((err, res) => {
+    })
+    app.get('/sql/settings/:setting_key', (req, res) => {
+        sqlManager.getSetting(req.params.setting_key, (err, sqlRes) => {
             if (err) {
-                console.log('Create table error.\n'+err.stack);
+                console.log('Error.\n' + err.stack);
             } else {
-                if(res == true) console.log('Table exists');
-                else  console.log('Create table success.\n' + JSON.stringify(res));
+                console.log('Success.\n' + JSON.stringify(sqlRes));
+                res.send(sqlRes);
             }
         });
     });
-}).catch(err => {
-    console.log('SQL connect fail.\n' + err.stack);
-})
+    app.post('/', linebotParser);
+
+    const connectSqlAsyncRun = async () => {
+        const client = new Client({ connectionString: process.env.DATABASE_URL, });
+        await client.connect();
+        //Init sql manager
+        sqlManager = new SqlManager(client);
+        return sqlManager;
+    }
+    connectSqlAsyncRun().then((sqlManager) => {
+        console.log('SQL connect success. App start.');
+        var server = app.listen(process.env.PORT || 8080, () => {
+            var port = server.address().port;
+            console.log("App now running on port", port);
+
+            sqlManager.createTables((err, res) => {
+                if (err) {
+                    console.log('Create table error.\n' + err.stack);
+                } else {
+                    if (res == true) console.log('Table exists');
+                    else console.log('Create table success.\n' + JSON.stringify(res));
+                }
+            });
+        });
+    }).catch(err => {
+        console.log('SQL connect fail.\n' + err.stack);
+    })
 
